49b961920db857cb2baffefdb1219615f2e33851
Aleksey Gordeyev <agordeyev94@gmail.com>
2022-07-27 20:38:57 +0500

    added Hometask1.py

---
+++ Hometask1.py
@@ -0,0 +1,9 @@
+square_side = int(input("Введите длину стороны квадрата: "))
+perimeter = square_side * 4
+square = square_side * square_side
+print(f'Вывод: \nПериметр: {perimeter} \nПлощадь:{square}')
+rectangle_height = int(input("Введите высоту прямоугольника: "))
+rectangle_width = int(input("Введите ширину прямоугольника: "))
+perimeter = rectangle_height * 2 + rectangle_width * 2
+square = rectangle_height * rectangle_width
+print(f'Вывод: \nПериметр: {perimeter} \nПлощадь:{square}')
